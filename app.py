4b5e15dc798d66de546638f27f47a72abe65714c
yasmin kadry <yasminkadry6720@gmail.com>
2025-06-08 15:30:24 +0300

    some updates

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import re
 import os
 import time
